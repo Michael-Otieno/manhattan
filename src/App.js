15a69079fcb0ed239dc7da4b5a76c8f85ecb3cab
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-09 12:59:29 +0300

    add search and filter components to app.js and style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+import Filter from "./components/filter/Filter";
 
 import Nav from './components/nav/Nav';
+import Search from "./components/search/Search";
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-      <div>
-        Search
-        filter
+      <div className='search-filter'>
+        <Search/>
+        <Filter/>
       </div>
     </div>
   );
