85468d5646e7e6bdfee1be5e24a12b5df6d92d75
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-10 09:51:03 +0300

    add imageslider and stylings

--- src/components/imageSlider/ImageSlider.js
+++ src/components/imageSlider/ImageSlider.js
@@ -1,8 +1,45 @@
-import './ImageSlider.css';
+import "./ImageSlider.css";
+import meals from "../../imgs/meals.png";
+import desert from "../../imgs/desert.png";
+import beverages from "../../imgs/beverages.png";
+import left from "../../imgs/left.png";
+import right from "../../imgs/right.png";
+import { useState } from "react";
 
-function ImageSlider(){
-  return(
-    <div>ImageSlider</div>
-  )
+function ImageSlider() {
+  const images = [meals,beverages,desert];
+
+  const [activeImage, setActiveImage] = useState(0)
+  // console.log(images)
+  function next(){
+    if(activeImage < 2){
+      setActiveImage(activeImage + 1)
+      
+    }else{
+      setActiveImage(2)
+    }
+  }
+  function previous(){
+    if(activeImage > 0){
+      setActiveImage(activeImage-1)
+    }else{
+      setActiveImage(0)
+    }
+  }
+
+  return (
+    <div className="image-slider">
+      <div className="left">
+        <img onClick={previous} src={right} alt='' />
+      </div>
+      <div className="imgs">
+        <img src={images[activeImage]}  alt=''/>
+      </div>
+      <div className="right">
+        <img onClick={next} src={left}  alt=''/>
+      </div>
+    </div>
+  );
 }
+
 export default ImageSlider;
