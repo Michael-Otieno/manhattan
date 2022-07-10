03b00f2c9b594d120e94869aa064f405394b4bc7
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-10 05:57:49 +0300

    add seat no details and styles

--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -2,8 +2,11 @@ import './Meals.css';
 
 function Meals(){
   return(
-    <div>
-      Meals
+    <div className='meals'>
+      <p className='seat-no'>
+        <span>Seat No:</span>
+        <input type='number' placeholder='000'/>
+      </p>
     </div>
   )
 }
