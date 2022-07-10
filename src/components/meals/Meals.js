dbc32b14429b41e93f24dd9b5620d0dc8bb74d41
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-10 15:36:18 +0300

    add meals, beverages and deserts with stylings

--- src/components/meals/Meals.js
+++ src/components/meals/Meals.js
@@ -1,15 +1,122 @@
-import ImageSlider from '../imageSlider/ImageSlider.js';
-import './Meals.css';
+import ImageSlider from "../imageSlider/ImageSlider.js";
+import "./Meals.css";
 
-function Meals(){
-  return(
-    <div className='meals'>
-      <p className='seat-no'>
+function Meals() {
+  return (
+    <div className="meals">
+      <p className="seat-no">
         <span>Seat No:</span>
-        <input type='number' placeholder='000'/>
+        <input type="number" placeholder="000" />
       </p>
-      <ImageSlider/>
+      <ImageSlider />
+      {/* meals */}
+      <div className="meals-list">
+        <div className="meals-on-list">
+          <h3>Meals</h3>
+          <ul>
+            <li>Chips</li>
+            <li>Grilled Chicken(1)</li>
+            <li>Grilled Chicken(1/2)</li>
+          </ul>
+        </div>
+        <div className="meals-price-list">
+          <h3>Price</h3>
+          <ul>
+            <li>100.00</li>
+            <li>600.00</li>
+            <li>300.00</li>
+          </ul>
+        </div>
+        <div className="meals-order-btn-list">
+          <h3>Add to list</h3>
+          <ul>
+            <li>
+              <button>Add to order</button>
+            </li>
+            <li>
+              <button>Add to order</button>
+            </li>
+            <li>
+              <button>Add to order</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* beverages */}
+      <div className="beverages-list">
+        <div className="beverages-on-list">
+          <h3>Beverages</h3>
+          <ul>
+            <li>Mango or Passion juice(500ml)</li>
+            <li>Mango or Passion juice(300ml)</li>
+            <li>Soda(500ml)</li>
+            <li>Pepsi Soda(500ml)</li>
+            <li>Pepsi Soda(300ml)</li>
+          </ul>
+        </div>
+        <div className="beverages-price-list">
+          <h3>Price</h3>
+          <ul>
+            <li>100.00</li>
+            <li>60.00</li>
+            <li>100.00</li>
+            <li>100.00</li>
+            <li>60.00</li>
+          </ul>
+        </div>
+        <div className="beverages-order-btn-list">
+          <h3>Add to list</h3>
+          <ul>
+            <li>
+              <button>Add to order</button>
+            </li>
+            <li>
+              <button>Add to order</button>
+            </li>
+            <li>
+              <button>Add to order</button>
+            </li>
+            <li>
+              <button>Add to order</button>
+            </li>
+            <li>
+              <button>Add to order</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* deserts */}
+      <div className="deserts-list">
+        <div className="deserts-on-list">
+          <h3>Deserts</h3>
+          <ul>
+            <li>Samosas</li>
+            <li>Sausages</li>
+          </ul>
+        </div>
+        <div className="desert-price-list">
+          <h3>Price</h3>
+          <ul>
+            <li>100.00</li>
+            <li>60.00</li>
+          </ul>
+        </div>
+        <div className="desert-order-btn-list">
+          <h3>Add to list</h3>
+          <ul>
+            <li>
+              <button>Add to order</button>
+            </li>
+            <li>
+              <button>Add to order</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+     
     </div>
-  )
+  );
 }
 export default Meals;
