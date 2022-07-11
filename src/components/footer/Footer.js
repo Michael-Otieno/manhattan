23c5b402949cfd1913b16a589edf5534ed79db6c
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-11 14:20:33 +0300

    add footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,48 @@
-import './Footer.css';
+import "./Footer.css";
+import restaurantOne from "../../imgs/rest.jpg";
+import restaurantTwo from "../../imgs/rest1.jpg";
+import restaurantThree from "../../imgs/rest2.jpg";
+import instagram from "../../imgs/ig.png";
+import facebook from "../../imgs/facebook.png";
+import tiktok from "../../imgs/tiktok.png";
 
-function Footer(){
-  return(
-    <div>Footer</div>
-  )
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-imgs">
+        <img src={restaurantOne} alt="" />
+        <img src={restaurantTwo} alt="" />
+        <img src={restaurantThree} alt="" />
+      </div>
+      <div className="footer-details">
+        <div className="footer-logo">
+          <p>Manhattan chicken</p>
+        </div>
+        <div className="footer-branches">
+          <h4>Our branches</h4>
+          <ul>
+            <li> - Eastleigh</li>
+            <li> - Kiamaiko</li>
+            <li> - Bama</li>
+            <li> - Tom Mboya</li>
+          </ul>
+        </div>
+        <div className="footer-online">
+          <h4>Follow us online</h4>
+          <ul>
+            <li>
+              <img src={facebook} alt="" /><span> - Facebook</span>
+            </li>
+            <li>
+              <img src={instagram} alt="" /><span> - Instagram</span>
+            </li>
+            <li>
+              <img src={tiktok} alt="" /><span>- TikTok</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Footer;
