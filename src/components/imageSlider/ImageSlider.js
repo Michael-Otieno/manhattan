c26c4d1d2cf5a25e94a3c55a5a6c71b93a00aa55
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-11 23:04:59 +0300

    remove image slide from desktop layout

--- src/components/imageSlider/ImageSlider.js
+++ src/components/imageSlider/ImageSlider.js
@@ -7,35 +7,37 @@ import right from "../../imgs/right.png";
 import { useState } from "react";
 
 function ImageSlider() {
-  const images = [meals,beverages,desert];
-  const [activeImage, setActiveImage] = useState(0)
-  function next(){
-    if(activeImage < 2){
-      setActiveImage(activeImage + 1)
-      
-    }else{
-      setActiveImage(2)
+  const images = [meals, beverages, desert];
+  const [activeImage, setActiveImage] = useState(0);
+  function next() {
+    if (activeImage < 2) {
+      setActiveImage(activeImage + 1);
+    } else {
+      setActiveImage(2);
     }
   }
-  function previous(){
-    if(activeImage > 0){
-      setActiveImage(activeImage-1)
-    }else{
-      setActiveImage(0)
+  function previous() {
+    if (activeImage > 0) {
+      setActiveImage(activeImage - 1);
+    } else {
+      setActiveImage(0);
     }
   }
 
   return (
-    <div className="image-slider">
-      <div className="left">
-        <img onClick={previous} src={right} alt='' />
+    <div>
+      <div className="image-slider">
+        <div className="left">
+          <img onClick={previous} src={right} alt="" />
+        </div>
+        <div className="imgs">
+          <img src={images[activeImage]} alt="" />
+        </div>
+        <div className="right">
+          <img onClick={next} src={left} alt="" />
+        </div>
       </div>
-      <div className="imgs">
-        <img src={images[activeImage]}  alt=''/>
-      </div>
-      <div className="right">
-        <img onClick={next} src={left}  alt=''/>
-      </div>
+      
     </div>
   );
 }
