9b208040b74c7a858b9aac1af1d1bb43e7e4d343
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-13 15:24:57 +0300

    remove search and filter from app.js to meals component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
-import Filter from "./components/filter/Filter";
 
 import Nav from './components/nav/Nav';
-import Search from "./components/search/Search";
+
 import Meals from "./components/meals/Meals";
 import Customer from "./components/customer/Customer";
 
@@ -12,10 +11,7 @@ function App() {
   return (
     <div className="App">
       <Nav/>
-      <div className='search-filter'>
-        <Search/>
-        <Filter/>
-      </div>
+      
       <Routes>
         <Route path="/" element={<Meals/>} />
         <Route path="/customer" element={<Customer/>} />
