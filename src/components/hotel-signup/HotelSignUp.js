33ebc33d60efed07c6d4bc8a0175f3a01257de07
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-14 09:37:30 +0300

    add hotel-signup form and style

--- src/components/hotel-signup/HotelSignUp.js
+++ src/components/hotel-signup/HotelSignUp.js
@@ -1,11 +1,29 @@
-import './HotelSignUp.css';
+import "./HotelSignUp.css";
+import { Link } from "react-router-dom";
 
-function HotelSignUp(){
+function HotelSignUp() {
   return (
-    <div>
-      HotelSignUp
+    <div className="signup">
+      <div>
+        <h4>Sign Up</h4>
+        <input type="text" placeholder="Company Name" />
+        <br />
+        <input type="email" placeholder="Email" />
+        <br />
+        <input type="password" placeholder="Password" />
+        <br />
+        <input type="password" placeholder="Confirm Password" />
+        <br />
+        <div className="sign-up-btn">
+          <button>Sign Up</button>
+          <p>
+            Already registered?
+            <Link to="/sign-in">Sign in</Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HotelSignUp;
