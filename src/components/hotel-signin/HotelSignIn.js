23b9938aa9ac887942e9f4b07a39454e25708149
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-14 09:53:20 +0300

    add sign in form and style

--- src/components/hotel-signin/HotelSignIn.js
+++ src/components/hotel-signin/HotelSignIn.js
@@ -1,9 +1,23 @@
 import './HotelSignIn.css';
+import {Link} from 'react-router-dom';
 
 function HotelSignIn(){
   return (
-    <div>
-      HotelSignIn
+    <div className="sign-in">
+      <form>
+        <h4>Sign In</h4>
+        <input type="email" placeholder="Email" />
+        <br />
+        <input type="password" placeholder="Password" />
+        <br />
+        <div className="sign-in-btn">
+          <button>Sign Up</button>
+          <p>
+            Not registered?
+            <Link to="/hotel-signup">Sign up</Link>
+          </p>
+        </div>
+      </form>
     </div>
   )
 }
