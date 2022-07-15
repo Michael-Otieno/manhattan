8a6bca518d2b9a504e0f0f32e1b59d361789094a
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-07-15 09:26:09 +0300

    center signup form

--- src/components/hotel-signup/HotelSignUp.js
+++ src/components/hotel-signup/HotelSignUp.js
@@ -3,25 +3,27 @@ import { Link } from "react-router-dom";
 
 function HotelSignUp() {
   return (
-    <div className="signup">
-      <form>
-        <h4>Sign Up</h4>
-        <input type="text" placeholder="Company Name" />
-        <br />
-        <input type="email" placeholder="Email" />
-        <br />
-        <input type="password" placeholder="Password" />
-        <br />
-        <input type="password" placeholder="Confirm Password" />
-        <br />
-        <div className="sign-up-btn">
-          <button>Sign Up</button>
-          <p>
-            Already registered?
-            <Link to="/sign-in">Sign in</Link>
-          </p>
-        </div>
-      </form>
+    <div className="center-form">
+      <div className="signup">
+        <form>
+          <h4>Sign Up</h4>
+          <input type="text" placeholder="Company Name" />
+          <br />
+          <input type="email" placeholder="Email" />
+          <br />
+          <input type="password" placeholder="Password" />
+          <br />
+          <input type="password" placeholder="Confirm Password" />
+          <br />
+          <div className="sign-up-btn">
+            <button>Sign Up</button>
+            <p>
+              Already registered?
+              <Link to="/sign-in">Sign in</Link>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
